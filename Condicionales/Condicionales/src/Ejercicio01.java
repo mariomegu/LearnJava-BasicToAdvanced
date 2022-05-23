12d77dad97ce0b56e1df028f48f08e8452ace887
mariomegu <mariomegu@gmail.com>
2022-05-23 18:10:04 -0500

    Resuelto el ejercicio 01 de las condicionales

---
+++ Condicionales/Condicionales/src/Ejercicio01.java
@@ -0,0 +1,25 @@
+import javax.swing.JOptionPane;
+
+public class Ejercicio01 {
+
+    public static void main (String[] args){
+
+        //Hacer un programa que lea un numero entero y muestre si el numero es múltiplo de 10
+
+        int numeroIngresado, residuo ;
+
+        JOptionPane.showMessageDialog(null, "Este programa muestra si un número ingresado es multiplo de 10");
+
+        numeroIngresado = Integer.parseInt(JOptionPane.showInputDialog("Digita un número"));
+
+        residuo = numeroIngresado%10;
+
+        if (residuo == 0){
+            JOptionPane.showMessageDialog(null, "El número ingresado es multiplo de 10");
+        } else {
+            JOptionPane.showMessageDialog(null, "El número ingresado NO es multiplo de 10");
+        }
+
+    }
+    
+}
